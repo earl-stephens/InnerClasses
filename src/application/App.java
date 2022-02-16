ed78c8293bf4674245e77fb03ff2deb377be67e8
earl-stephens <stephensearl@hotmail.com>
2022-02-15 23:24:18 -0700

    Demonstrate non static inner class usage

--- src/application/App.java
+++ src/application/App.java
@@ -3,8 +3,15 @@ package application;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Person person = new Person("Sigmund");
+		person.display();
+		
+		/* The syntax for instantiating an inner non static
+		 * class object is different.  Can't do Person.Printer.  
+		 * Printer has to be associated with objects.  In this case
+		 * need a Person object
+		 */
+		Person.Printer printers = person.new Printer();
 	}
 
 }
